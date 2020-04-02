d0fbd6be0bbed7cbd60e7a3c0ed94f3737f04e81
Craig Swanson <cswanson02@me.com>
2020-04-02 00:15:32 -0500

    Update GGATimedTask.m
    Completed initializer in the implementation file.

--- TimeTracker/Model/GGATimedTask.m
+++ TimeTracker/Model/GGATimedTask.m
@@ -10,4 +10,22 @@
 
 @implementation GGATimedTask
 
+- (instancetype)initWithString:(NSString *)clientName
+                   workSummary:(NSString *)workSummary
+                          rate:(double)rate
+                         hours:(double)hours {
+    self = [super init];
+    if (self) {
+        _clientName = clientName;
+        _workSummary = workSummary;
+        _rate = rate;
+        _hours = hours;
+    }
+    return self;
+}
+
+- (double)totalCharged {
+    return self.hours * self.rate;
+}
+
 @end
