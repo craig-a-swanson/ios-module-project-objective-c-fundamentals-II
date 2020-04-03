70439aa1873ecd687b3d229accaaa504142eed82
Craig Swanson <cswanson02@me.com>
2020-04-03 10:45:28 -0500

    Completed Project MVP
    Implemented cell updates and the logTime action to update and create a new timedTask.

--- TimeTracker/Model/GGATimeTrackerViewController.m
+++ TimeTracker/Model/GGATimeTrackerViewController.m
@@ -37,9 +37,29 @@
 
 // MARK: - Actions
 - (IBAction)logTime:(UIButton *)sender {
+    NSString *clientName = _clientNameTextField.text;
+    NSString *workSummary = _workSummaryTextField.text;
+    double hourlyRate = [_hourlyRateTextField.text doubleValue];
+    double hoursWorked = [_hoursWorkedTextField.text doubleValue];
     
+    [self.timedTaskController createTimedTaskWith:clientName
+                                          summary:workSummary
+                                          Payrate:hourlyRate
+                                      hoursWorked:hoursWorked];
+    
+    [self.tableView reloadData];
+    [self updateViews];
 }
 
+// MARK: - Methods
+- (void)updateViews {
+    _clientNameTextField.text = @"";
+    _workSummaryTextField.text = @"";
+    _hourlyRateTextField.text = @"";
+    _hoursWorkedTextField.text = @"";
+}
+
+// MARK: - TableView Datasource
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return self.timedTaskController.timedTasks.count;
 }
@@ -48,7 +68,9 @@
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TitleCell" forIndexPath:indexPath];
     
-    
+    GGATimedTask *task = [self.timedTaskController.timedTasks objectAtIndex:indexPath.row];
+    cell.textLabel.text = task.clientName;
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"$%0.2f", task.totalCharged];
     
     return cell;
 }
