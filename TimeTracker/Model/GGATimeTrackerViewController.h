c0cf0a18ca53f32cde5193d4ffa6ad76083d19be
Craig Swanson <cswanson02@me.com>
2020-04-03 00:05:56 -0500

    Updated View Controller
    Created UI and tableView outlets and wired them up.

---
+++ TimeTracker/Model/GGATimeTrackerViewController.h
@@ -0,0 +1,17 @@
+//
+//  GGATimeTrackerViewController.h
+//  TimeTracker
+//
+//  Created by Craig Swanson on 4/2/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GGATimeTrackerViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/Model/GGATimeTrackerViewController.m
@@ -0,0 +1,56 @@
+//
+//  GGATimeTrackerViewController.m
+//  TimeTracker
+//
+//  Created by Craig Swanson on 4/2/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "GGATimeTrackerViewController.h"
+#import "GGATimedTask.h"
+#import "GGATimedTaskController.h"
+
+@interface GGATimeTrackerViewController ()
+
+// MARK: - Properties
+@property (nonatomic) GGATimedTaskController *timedTaskController;
+
+// MARK: - Outlets
+@property (strong, nonatomic) IBOutlet UITextField *clientNameTextField;
+@property (strong, nonatomic) IBOutlet UITextField *workSummaryTextField;
+@property (strong, nonatomic) IBOutlet UITextField *hourlyRateTextField;
+@property (strong, nonatomic) IBOutlet UITextField *hoursWorkedTextField;
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation GGATimeTrackerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.timedTaskController = [[GGATimedTaskController alloc]  init];
+    
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+}
+
+// MARK: - Actions
+- (IBAction)logTime:(UIButton *)sender {
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
+// MARK: - UITableViewDelegate
+//- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+//
+//}
+
+@end
